4a690488c07f3b594d2ed6929cc0db930a7b6029
ashwabu <abdulazizredi90@gmail.com>
2023-07-12 17:32:57 +0300

    writing the image

---
+++ script.py
@@ -0,0 +1,14 @@
+import cv2
+
+import glob
+
+
+images = glob.glob('*.jpg')
+
+for image in images:
+    img=cv2.imread(image,0)
+    rs=cv2.resize(img,(100,100))
+    cv2.imshow('images/',rs)
+    cv2.waitKey(2000)
+    cv2.destroyAllWindows()
+    
